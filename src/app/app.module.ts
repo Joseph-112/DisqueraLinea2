a7bdf59b0cf5e0d0a2f1562afda65bf2008c92d2
Joseph-112 <44301921+Joseph-112@users.noreply.github.com>
2021-11-09 18:18:46 -0500

    Elimino componentes innecesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AdministradorComponent } from './administrador/administrador.component';
-import { ProgFechasComponent } from './administrador/prog-fechas/prog-fechas.component';
-import { ResultadosComponent } from './administrador/resultados/resultados.component';
-import { DocenteComponent } from './docente/docente.component';
-import { EncuestaComponent } from './docente/encuesta/encuesta.component';
-import { EstudianteComponent } from './estudiante/estudiante.component';
-import { DocumentosComponent } from './administrador/documentos/documentos.component';
 import { AdminServiceService } from './admin-service.service';
 import { AppRoutingModule} from './app-routing.module';
 
@@ -22,13 +16,7 @@ const routes: Routes = [];
 @NgModule({
   declarations: [
     AppComponent,
-    AdministradorComponent,
-    ProgFechasComponent,
-    ResultadosComponent,
-    DocenteComponent,
-    EncuestaComponent,
-    EstudianteComponent,
-    DocumentosComponent
+    AdministradorComponent
   ],
   imports: [
     BrowserModule,
