fda9b9218daa2b891728a31ded0b52063aaec767
Joseph-112 <44301921+Joseph-112@users.noreply.github.com>
2021-11-16 22:09:42 -0500

    Login

---
+++ src/app/login/login/login.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component'; 
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterModule,
+    FlexLayoutModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatDialogModule
+  ]
+})
+export class LoginModule { }
