e31262e6137e2fa79178cdbd1d638ae2a96afde0
ivaaan06 <62316002+ivaaan06@users.noreply.github.com>
2021-11-19 00:32:11 -0500

    Union

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DefaultComponent } from './../../administrador/default/default.component';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openDialogAdministrador() {
+    const dialogRef = this.dialog.open( DefaultComponent);
   }
  
 }
