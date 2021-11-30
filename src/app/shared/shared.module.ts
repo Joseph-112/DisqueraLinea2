8ac7842fb08d79f7b903f59b6dab52a4c9de9da8
Joseph-112 <44301921+Joseph-112@users.noreply.github.com>
2021-11-29 21:56:15 -0500

    esto no sirve

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,7 +8,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu'; 
 import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -89,7 +89,7 @@ import { CarritoComponent } from './widgets/catalogo/carrito/carrito.component';
     FormAlbumVerArtistaComponent,
     FormAlbumVerCancionesComponent,
     FormEditarAlbumComponent,
-    CarritoComponent,
+    CarritoComponent
     
     
     
@@ -127,8 +127,9 @@ import { CarritoComponent } from './widgets/catalogo/carrito/carrito.component';
     BoxComponent,
     CardComponent,
     CalendarioComponent,
-    CardAlbumComponent,
-    CatalogoComponent
+    CardAlbumComponent, 
+    CatalogoComponent,
+    CarritoComponent
   ]
 })
 export class SharedModule { }
