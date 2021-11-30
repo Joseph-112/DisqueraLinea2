bfb0bfd025b1fc3522a6736897012dcb0174c063
Joseph-112 <44301921+Joseph-112@users.noreply.github.com>
2021-11-29 22:16:03 -0500

    Esto no sirve pero shared ya no tiene errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { LoginModule } from './login/login/login.module';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login/login.component';
 
 
 
@@ -22,7 +23,8 @@ const routes: Routes = [];
 @NgModule({
   declarations: [
     AppComponent,
-    AdministradorComponent
+    AdministradorComponent,
+    LoginComponent
   ],
   imports: [
   
